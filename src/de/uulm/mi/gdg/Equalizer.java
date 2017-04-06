dde6697e68bebf3b17e35200ee98b455f03d2940
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2017-04-06 19:38:17 +0200

    Add 10 circles to the animation and display them

--- src/de/uulm/mi/gdg/Equalizer.java
+++ src/de/uulm/mi/gdg/Equalizer.java
@@ -1,6 +1,10 @@
 package de.uulm.mi.gdg;
 
+import de.uulm.mi.gdg.objects.Circle;
 import processing.core.PApplet;
+import processing.core.PVector;
+
+import java.util.ArrayList;
 
 /**
  * The Equalizer class is the main controller for the Animation.
@@ -8,6 +12,9 @@ import processing.core.PApplet;
  * @author Tobias Lahmann
  */
 public class Equalizer extends PApplet {
+    // A list of circles to hold the elements to display.
+    private ArrayList<Circle> circles;
+
     /**
      * The settings() method runs before the sketch has been set up, so other Processing functions cannot be used at
      * this point.
@@ -20,14 +27,29 @@ public class Equalizer extends PApplet {
      * The setup method is called before displaying any objects. The Method calls commands in the Processing API.
      */
     public void setup() {
+        circles = new ArrayList<>();
 
+        // Creates random circles to see what an animation could look like
+        for (int i = 0; i < 10; i++) {
+            float xPosition = random(this.width);
+            float yPosition = random(this.height);
+            PVector position = new PVector(xPosition, yPosition);
+            float radius = random(10, 100);
+            int color = color(0, 128, 128);
+            Circle c = new Circle(position, radius, color);
+            circles.add(c);
+        }
     }
 
     /**
      * Draw method is responsible for displaying all objects.
      */
     public void draw() {
-
+        // Display every circle available
+        for (Circle c : circles) {
+            stroke(c.color);
+            ellipse(c.position.x, c.position.y, c.radius, c.radius);
+        }
     }
 
     /**
