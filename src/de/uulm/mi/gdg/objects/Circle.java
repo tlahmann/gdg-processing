17c7891adb77abff07f394fd8ceed3260adf256d
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2017-04-07 00:15:52 +0200

    Make circles to arcs and add rotaion

--- src/de/uulm/mi/gdg/objects/Circle.java
+++ src/de/uulm/mi/gdg/objects/Circle.java
@@ -12,11 +12,13 @@ public class Circle {
     private static PApplet canvas;
     private PShape shape;
     private PVector position;
+    private float angle;
+    private float speed;
 
     /**
      * Constructor for the Circle. Creates a PShape.
      *
-     * @param papa   The PApplet to draw on
+     * @param papa     The PApplet to draw on
      * @param position The position of the circle
      * @param radius   The radius of the circle
      * @param color    The color of the circles stroke
@@ -26,19 +28,39 @@ public class Circle {
         this.position = position;
 
         // Shapes bring the advantage of being easily modifiable and adjustable
-        shape = canvas.createShape(PConstants.ELLIPSE, 0, 0, radius, radius);
-        shape.setStroke(color);
-        shape.setStrokeWeight(weight);
-        shape.setFill(false);
+        shape = canvas.createShape();
+        shape.beginShape();
+
+        shape.stroke(color);
+        shape.strokeWeight(weight);
+        shape.noFill();
+
+        // The arc itself. The span variable sets the dimension of the arc to a random value starting from quarter pi
+        // because arcs that are too small are not pretty. I'm their arch nemesis
+        float span = canvas.random(PConstants.QUARTER_PI, PConstants.TWO_PI);
+        for (float a = -span; a < 0; a += 0.1) {
+            shape.vertex(PApplet.cos(a) * radius / 2, PApplet.sin(a) * radius / 2);
+        }
+
+        this.angle = canvas.random(PConstants.TWO_PI);
+        shape.rotate(angle);
+
+        shape.endShape();
+
+        int direction = -1 + (int) canvas.random(2) * 2; // results in either a 1 or -1
+        this.speed = direction * canvas.random(0.003f, 0.007f);
     }
 
     /**
      * Displays the shape on the stored static canvas object.
      */
     public void display() {
+        this.angle = (this.angle + this.speed) % PApplet.TWO_PI;
+
         canvas.pushMatrix();
 
         canvas.translate(position.x, position.y);
+        canvas.rotate(angle);
         canvas.shape(shape);
 
         canvas.popMatrix();
